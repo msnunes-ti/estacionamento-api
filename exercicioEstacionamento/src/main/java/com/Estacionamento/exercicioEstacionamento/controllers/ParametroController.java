3661fe89e97a0c19d6d883288bbaba8937507a2a
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-07-11 21:51:20 -0300

    atualizado

--- exercicioEstacionamento/src/main/java/com/Estacionamento/exercicioEstacionamento/controllers/ParametroController.java
+++ exercicioEstacionamento/src/main/java/com/Estacionamento/exercicioEstacionamento/controllers/ParametroController.java
@@ -2,21 +2,34 @@ package com.Estacionamento.exercicioEstacionamento.controllers;
 
 import com.Estacionamento.exercicioEstacionamento.dto.CadastraParametroDTO;
 import com.Estacionamento.exercicioEstacionamento.model.Parametro;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/parametros")
 public class ParametroController {
 
-    @Autowired
     CadastraParametroDTO cadastraParametroDTO;
 
+    @PostMapping
+    public void criaParametro(Double valor) {
+        List<Parametro> parametros = (List<Parametro>) cadastraParametroDTO.consultaTodos();
+        if (parametros.size() == 0) {
+            cadastraParametroDTO.criaParametro(BigDecimal.valueOf(valor));
+        }
+        throw new RuntimeException("O Parametro já está criado então, somento pode ser alterado!");
+    }
+
     @GetMapping
-    public void consultaParametroDTO() {
-        cadastraParametroDTO.consultaTodos();
+    public Iterable<Parametro> consultaParametroDTO() {
+        List<Parametro> parametros = (List<Parametro>) cadastraParametroDTO.consultaTodos();
+        if (parametros.size() == 0) {
+            throw new RuntimeException("Não foram encontrados parametros cadastrados.");
+        }
+        return parametros;
     }
 
     @GetMapping(path = "/{codigo}")
@@ -24,9 +37,6 @@ public class ParametroController {
         return cadastraParametroDTO.consultaPorId(id);
     }
 
-    @PostMapping
-    public void alteraParametro(Long id, Double valor) {
 
-    }
 
 }
--- exercicioEstacionamento/src/main/java/com/Estacionamento/exercicioEstacionamento/dto/CadastraParametroDTO.java
+++ exercicioEstacionamento/src/main/java/com/Estacionamento/exercicioEstacionamento/dto/CadastraParametroDTO.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 public class CadastraParametroDTO {
 
-    @Autowired
+    @Autowired(required = true)
     private ParametroRepository parametroRepository;
 
     @NotNull
